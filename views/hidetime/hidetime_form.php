23d7afed83c5c8bf42f2d10cfac8624d3f84fc8d
jetherton <john.etherton@gmail.com>
2011-03-30 10:19:08 -0600

    Added a brief explanation of what this does in the UI

--- views/hidetime/hidetime_form.php
+++ views/hidetime/hidetime_form.php
@@ -5,6 +5,7 @@
 		Hide Time:		
 		<input type="checkbox" name="hidetime" id="hidetime" value="<?php echo $hide; ?>" 
 		<?php echo ($hide ? 'checked="checked"' : ''); ?> onclick="toggleHideTime('<?php echo url::site(); ?>', '<?php echo $report_id; ?>'); "/>
+		<span>Checking this will hide the time.</span>
 	</h4>	
 </div>
 
