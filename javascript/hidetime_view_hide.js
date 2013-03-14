168472a2d6931f89ecfab906496f7c469100fd95
John Etherton <john.etherton@gmail.com>
2013-03-14 17:28:47 -0600

    Now works with Ushahidi 2.6.1

--- javascript/hidetime_view_hide.js
+++ javascript/hidetime_view_hide.js
@@ -3,7 +3,9 @@
 //Java script added by the hide time plugin to make sure that the time is hidden
 $().ready(
 	function(){ 
-		$('span.r_date' ).css("display", "none");
+		var timeStr = $('span.r_date' ).html();
+		console.log(timeStr);
+		$('span.r_date' ).html(timeStr.substring(5));
 		
 		$('#datenotime' ).css("display", "none");
 	});
