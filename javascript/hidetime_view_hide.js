1060ea35d1c061427e6054f78d0691e0a1df340e
John Etherton <john.etherton@gmail.com>
2013-03-15 09:51:52 -0600

    more inteligently removes time.

--- javascript/hidetime_view_hide.js
+++ javascript/hidetime_view_hide.js
@@ -4,8 +4,12 @@
 $().ready(
 	function(){ 
 		var timeStr = $('span.r_date' ).html();
-		console.log(timeStr);
-		$('span.r_date' ).html(timeStr.substring(5));
+		var index = timeStr.indexOf(':');
+		timeStr = timeStr.substring(0,index-2) + timeStr.substring(index+3);
+
+
+
+		$('span.r_date' ).html(timeStr);
 		
 		$('#datenotime' ).css("display", "none");
 	});
